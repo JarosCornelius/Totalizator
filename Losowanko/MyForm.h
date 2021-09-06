334a63ed158a04657f5a723eed4c0ffbc2c1718a
Karol <karoljanjaros@gmail.com>
2021-09-06 18:36:27 +0200

    Dodaj pliki projektów.

---
+++ Losowanko/MyForm.h
@@ -0,0 +1,198 @@
+#pragma once
+#include <ctime>
+#include <cstdlib>
+
+namespace Losowanko {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label6;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Transparent;
+			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Transparent;
+			this->button1->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Transparent;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->button1->Location = System::Drawing::Point(-14, 205);
+			this->button1->Margin = System::Windows::Forms::Padding(6);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(156, 49);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"LOSUJ";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Location = System::Drawing::Point(83, 30);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(21, 24);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"\?";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Location = System::Drawing::Point(29, 45);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(21, 24);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"\?";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->Location = System::Drawing::Point(117, 65);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(21, 24);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"\?";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::Color::Transparent;
+			this->label4->Location = System::Drawing::Point(38, 107);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(21, 24);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"\?";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->BackColor = System::Drawing::Color::Transparent;
+			this->label5->Location = System::Drawing::Point(68, 76);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(21, 24);
+			this->label5->TabIndex = 5;
+			this->label5->Text = L"\?";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->BackColor = System::Drawing::Color::Transparent;
+			this->label6->Location = System::Drawing::Point(97, 113);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(21, 24);
+			this->label6->TabIndex = 6;
+			this->label6->Text = L"\?";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 24);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(250, 272);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Margin = System::Windows::Forms::Padding(6);
+			this->Name = L"MyForm";
+			this->Text = L"Losowanko";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+	int numerki[6] = { 0 };
+
+	srand((int)time(0));
+
+	for (int l = 0; l < 6; l++)
+	{
+		numerki[l] = (rand() % 48) + 1;
+	}
+
+	label1->Text = Convert::ToString(numerki[0]);
+	label2->Text = Convert::ToString(numerki[1]);
+	label3->Text = Convert::ToString(numerki[2]);
+	label4->Text = Convert::ToString(numerki[3]);
+	label5->Text = Convert::ToString(numerki[4]);
+	label6->Text = Convert::ToString(numerki[5]);
+
+
+}
+};
+}
